7cbd20b8d851cd32a997759d981b4f31da083485
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-09-10 03:57:03 +0530

    update routing /

--- src/Pages/Error/Error.js
+++ src/Pages/Error/Error.js
@@ -8,16 +8,16 @@ const Error = () => {
   return (
     <>
       <div className="h-screen flex flex-col justify-center items-center">
-        <div className="text-6xl font-bold text-white">404</div>
-        <div className="text-xl text-white mt-4">
+        <div className="text-6xl font-bold text-inherit">404</div>
+        <div className="text-xl text-inherit mt-4">
           Oops! Something went wrong.
         </div>
-        <div className="text-md text-white mt-4">
+        <div className="text-md text-inherit mt-4">
           Sorry ! Page Not Found.
         </div>
         <Link
           to="/"
-          className="mt-8 border-2 shadow-lg border-white-600 p-2 rounded-md font-semibold text-white hover:border-red-600 hover:border-2 transition-all duration-300 ease-in-out hover:tracking-wider"
+          className="mt-8 border-2 border-black shadow-lg border-white-600 p-2 rounded-md font-semibold text-inherit hover:border-yellow-500 hover:border-2 transition-all duration-300 ease-in-out hover:tracking-wider"
         >
           Back to Home &rarr;
         </Link>
