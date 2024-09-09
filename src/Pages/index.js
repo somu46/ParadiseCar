42ba4415efcb381757b5aeb5fb5e084c9473b98a
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-09-10 03:37:42 +0530

    create error page

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,9 +1,9 @@
 import Home from "./Home/Home";
 import ContactUs from "./Contact/contactUs";
 import AboutUs from "./About/AbouUs";
+import Error from "./Error/Error";
 
 
 
 
-
-export { Home,ContactUs,AboutUs};
+export { Home,ContactUs,AboutUs,Error};
