84593646022489281a208c47673a601fdc9c98e9
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-09-13 14:06:40 +0530

    call booknow component

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,11 @@
-
-
-
+import BookNowCom from "../../Components/BookNowComponent/BookNowCom";
 
 const Home = () => {
   return (
     <>
-      <div className=" mt-[10rem] min-h-screen flex justify-center font-semibold text-3xl ">
-      <p className="cursor-pointer transition ease-in duration-300 hover:scale-110">This is Home Page !</p> 
+      <div className=" mt-[10rem] min-h-screen flex flex-col items-center p-3 my-5 ">
+      <p className=" my-5 cursor-pointer transition ease-in duration-300  font-semibold text-3xl hover:scale-110">This is Home Page !</p>
+      <BookNowCom/> 
       </div>
     </>
   );
