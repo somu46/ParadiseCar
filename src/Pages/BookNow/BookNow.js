23fceee31b226d964970dbdff2c5d3692ac5b578
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2024-09-21 20:49:44 +0530

    use react-hook-from and collect from data from the booknow page components

--- src/Pages/BookNow/BookNow.js
+++ src/Pages/BookNow/BookNow.js
@@ -1,23 +1,45 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import BookNowButton from "../../Components/Button/Button";
 import BookNowCarImg from "./../../Assets/BookNowCarImg.svg";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-
-
-
-const BookNowPageCom=()=>{
+const BookNowPageCom = () => {
   const [selectedPicupDate, setSelectedpicupDate] = useState(null);
   const [selectedDropoffDate, setSelectedDropoffDate] = useState(null);
-  const [airportOpt, setAirportOpt] = useState("");
+  const [airportOpt, setAirportOpt] = useState(" ");
 
-  
-  const handleSelectChange = (value) => {
-    setAirportOpt(value);
-  };
-  const handelSubmit = () => {
-    alert("Book Now open .....");
+  // for srom data submmistion
+
+  const [err, setErr] = useState("");
+  const {
+    register,
+    setValue, //manualy update value
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+
+  // const handleSelectChange = (value) => {
+  //   setAirportOpt(value);
+  // };
+  const FromData = (data) => {
+    setErr(""); // at frist we counter error as empty
+
+    try {
+      console.log(data);
+      console.log("seledPicupDate",selectedPicupDate)
+      console.log("DropoffDate",selectedDropoffDate)
+      console.log("airportOpt",airportOpt)
+      
+       
+    } catch (error) {
+      setErr(error);
+      console.log(err);
+      
+    }
+    // alert("selectedDropoffDate : " + typeof selectedDropoffDate);
   };
 
   return (
@@ -27,36 +49,58 @@ const BookNowPageCom=()=>{
           {/* Left Section (Form & Text) */}
           <div className="w-full md:w-1/2 flex flex-col justify-start p-5">
             <div className="mb-6">
-              <p className="text-3xl font-bold text-orange-600 mb-2">
+              <p className="text-3xl font-bold text-start text-orange-600 mb-2">
                 Booking for Outstation Trip
               </p>
-              <p className="text-lg font-semibold text-slate-900">
+              <p className="text-lg font-semibold text-start text-slate-900">
                 Estimated Fare ₹ 11.00 per/km
               </p>
             </div>
-            {/* Form */}
-            <form className="flex flex-wrap flex-col gap-4">
+                  {/* Form */}
+
+
+
+            <form
+              className="flex flex-wrap flex-col gap-4"
+              onSubmit={handleSubmit(FromData)}
+            >
               <div className="flex justify-between gap-4">
                 <input
                   type="text"
                   placeholder="Full Name"
                   className="p-3 border-2 border-gray-300 rounded-lg w-1/2"
+                  {...register("fullName",{
+                    required:true
+                  })}
+
                 />
+               {errors.fullName && <p className="absolute font-semibold text-lg  text-orange-600 ml-3 mt-[2.7rem] p-1 "> *Name is required.</p>}
                 <input
                   type="text"
                   placeholder="10 Digit Mobile No"
                   className="p-3 border-2 border-gray-300 rounded-lg w-1/2"
+                  {...register("MobileNum",{
+                    required:true,
+                  })}
                 />
               </div>
               <input
                 type="text"
                 placeholder="Enter your Destination"
                 className="p-3 border-2 border-gray-300 rounded-lg"
+                {...register("destinationLocation",{
+                  required:true,
+                })}
               />
               <input
                 type="text"
                 placeholder="Pick up Address"
                 className="p-3 border-2 border-gray-300 rounded-lg"
+                {
+                  ...register("PicUpLocation",{
+                    required:true,
+                  })
+                }
               />
 
               <div className="flex lg:flex-nowrap flex-wrap  justify-between gap-4">
@@ -64,42 +108,69 @@ const BookNowPageCom=()=>{
                   <label className="px-3 text-start">Pickup Date</label>
                   <DatePicker
                     selected={selectedPicupDate}
-                    onChange={(date) => setSelectedpicupDate(date)}
+                    onChange={(date) =>
+                       {
+                        setSelectedpicupDate(date);
+                        setValue("PickDateTime", date);
+                      }
+                      }
                     showTimeSelect
                     timeFormat="hh:mm aa"
                     timeIntervals={15}
                     dateFormat="MMMM d, yyyy h:mm aa"
                     className="p-3 border-2 border-gray-300 rounded-lg "
                     placeholderText="Select Pickup Date and Time"
+                    // {
+                    //   ...register("PickDatetime",{  //this is not support of this object
+                    //     required:true,
+                    //   })
+                    // }
                   />
-                  {/* <input
-                  type="datetime-local"
-                  placeholder="Pickup Date"
-                  className="p-3 border-2 border-gray-300 rounded-lg "
-                /> */}
+                
                 </div>
                 <div className=" flex flex-wrap flex-col w-full">
                   <label className="px-3 text-start">DropOff Date</label>
 
                   <DatePicker
                     selected={selectedDropoffDate}
-                    onChange={(date) => setSelectedDropoffDate(date)}
+                    onChange={(date) =>{ 
+                      setSelectedDropoffDate(date);
+                      setValue("dropOffDateTime",date)
+                      
+                    }}
                     showTimeSelect
                     timeFormat="hh:mm aa"
                     timeIntervals={15}
                     dateFormat="MMMM d, yyyy h:mm aa"
                     className="p-3 border-2 border-gray-300 rounded-lg"
                     placeholderText="Select Drop Date "
+                    // {
+                    //   ...register("dropoffDatetime",{
+                    //     required:true,
+                    //   })
+                    // }
                   />
                 </div>
               </div>
+
 
               <div className="flex flex-wrap flex-col">
                 <label className="text-start">Trip Type</label>
 
                 <select
-                  onChange={(e) => handleSelectChange(e.target.value)}
+                  onChange={(e) => {
+                    const tripDate=e.target.value;
+                    setAirportOpt(tripDate);
+                    setValue("tripype",tripDate)
+                  }
+                   }
                   className="p-3 border-2 border-gray-300 rounded-lg"
+                // {
+                //   ...register("tripype",{
+                //     required:true             //rgister and ongange event can't use in same time
+                //   }
+                //   )
+                // }
                 >
                   <option>Please select a Trip</option>
                   <option value="oneWay">ONE WAY</option>
@@ -108,7 +179,13 @@ const BookNowPageCom=()=>{
                 </select>
 
                 {airportOpt === "airPort" && (
-                  <select className=" flex items-center mt-5 justify-center p-3 border-2 max-w-[50%] border-gray-300 rounded-lg">
+                  <select className=" flex items-center mt-5 justify-center p-3 border-2 max-w-[50%] border-gray-300 rounded-lg"
+                  {
+                    ...register("airPortOptation",{
+                      required:true,
+                    })
+                  }
+                  >
                     <option>Please select type</option>
                     <option value="DropToAirport">Drop to Airport</option>
                     <option value="PickFromAirport">Pickup from Airport</option>
@@ -117,7 +194,7 @@ const BookNowPageCom=()=>{
               </div>
 
               <div>
-                <BookNowButton onClick={handelSubmit} label="Rent Now " />
+                <BookNowButton label="Rent Now " />
               </div>
             </form>
           </div>
@@ -126,7 +203,7 @@ const BookNowPageCom=()=>{
           <div className=" md:flex md:w-1/2  right-0 top-0 md:overflow-hidden">
             <img
               src={BookNowCarImg}
-              alt="Book Now Car"
+              alt="Book Now poster"
               className="min-w-full lg:min-h-full object-cover shadow-md rounded-md"
             />
           </div>
@@ -134,22 +211,18 @@ const BookNowPageCom=()=>{
       </div>
     </>
   );
-}
-
-
-const BookNow = () => {
-  
-return(
-  <>
-  <div className="border-2  shadow-sm rounded-md mt-[6rem] min-w-full min-h-screen flex flex-col items-center p-3 my-5">
-  <BookNowPageCom/>
-  </div>
-  
-  </>
-);
-  
 };
 
-export {BookNowPageCom};
+const BookNow = () => {
+  return (
+    <>
+      <div className="border-2  shadow-sm rounded-md mt-[6rem] min-w-full min-h-screen flex flex-col items-center p-3 my-5">
+        <BookNowPageCom/>
+      </div>
+    </>
+  );
+};
+
+export { BookNowPageCom };
 
 export default BookNow;
