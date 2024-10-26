30d407b8411c90ba357a04ceea7dec7c2b672a8f
Soumyajit Ray <soumyajitray2002@gmail.com>
2024-10-27 00:08:48 +0530

    BookNow page Added

---
+++ src/Pages/Booking/bookNow.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './bookNow.css'
+import Booknow from '../../Components/BookNowComponent/BookNowPageCom'
+const bookNow = () => {
+  return (
+    <div>
+        <div className="book-now">
+            <Booknow/>
+        </div>
+
+    </div>
+  )
+}
+
+export default bookNow
